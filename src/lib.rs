367bf88d90b2a37b70cb333b871a389383f98b10
jeremymarch <jmarch@gradcenter.cuny.edu>
2021-09-23 18:31:08 -0400

    rough, but mostly working strip_ending function

--- src/lib.rs
+++ src/lib.rs
@@ -89,20 +89,89 @@ trait HcVerbForms {
     fn explain_form(&self) -> String;
     fn get_pp_num(&self) -> HcGreekPrincipalParts;
     fn get_pp(&self) -> String;
+    fn strip_ending(&self, pp_num:usize, form:String) -> Vec<String>;
 }
-
+/*
+//https://stackoverflow.com/questions/59330671/how-do-i-remove-a-single-trailing-string-from-another-string-in-rust
+fn remove_suffix<'a>(s: &'a str, p: &str) -> &'a str {
+    if s.ends_with(p) {
+        &s[..s.len() - p.len()]
+    } else {
+        s
+    }
+}
+*/
 impl HcVerbForms for HcGreekVerbForm<'_> {
     /*
     fn new() -> HcGreekVerbForm {
 
     }*/
 
+    fn strip_ending(&self, pp_num:usize, form:String) -> Vec<String> {
+        let mut r = Vec::new();
+        match pp_num {
+            1..=2 => {
+                if form.ends_with("ω") {
+                    r.push(form.strip_suffix("ω").unwrap().to_string());
+                }
+                else if form.ends_with("ομαι") {
+                    r.push(form.strip_suffix("ομαι").unwrap().to_string());
+                }
+                else if form.ends_with("μι") {
+                    r.push(form.strip_suffix("μι").unwrap().to_string());
+                }
+            },
+            3 => {
+                if form.ends_with("αμην") {
+                    r.push(form.strip_suffix("αμην").unwrap().to_string());
+                }
+                else if form.ends_with("α") {
+                    r.push(form.strip_suffix("α").unwrap().to_string());
+                }
+            },
+            4 => {
+                if form.ends_with("α") {
+                    r.push(form.strip_suffix("α").unwrap().to_string());
+                }
+            },
+            5 => {
+                if form.ends_with("μαι") {
+                    r.push(form.strip_suffix("μαι").unwrap().to_string());
+                }
+            },
+            6 => {
+                if form.ends_with("ην") {
+                    r.push(form.strip_suffix("ην").unwrap().to_string());
+                }
+            },
+            _ => { r.push(form); }   
+        }
+        r
+    }
+
     fn get_form(&self) -> Vec<Step> {
-        let mut a = Vec::new();
-        let b = self.verb.pps;
-        let c = "blah2".to_string();
-        a.push(Step{form:b,explanation:c});
-        a
+        let mut steps = Vec::new();
+        let f = self.verb.pps.join(", ");
+        let e = "Principal Parts".to_string();
+        steps.push(Step{form:f, explanation:e});
+
+        let pp_num = self.get_pp_num() as usize;
+        let f = &self.verb.pps[pp_num - 1];
+        let e = "Choose Principal Part".to_string();
+        steps.push(Step{form:f.to_string(), explanation:e});
+
+        //internally (not as a step) strip accent
+/*
+        let each_alt = f.split(" / ");
+        for x in each_alt {
+
+        }
+*/
+        let f = self.strip_ending(pp_num, f.to_string()).join(",");
+        let e = "Remove ending from Principal Part".to_string();
+        steps.push(Step{form:f, explanation:e});
+
+        steps
     }
 
     fn explain_form(&self) -> String {
@@ -165,7 +234,9 @@ mod tests {
         let b = HcGreekVerbForm {verb:&a, person:HcPerson::First, number:HcNumber::Singular, tense:HcTense::Aorist, voice:HcVoice::Active, mood:HcMood::Indicative, gender:None};
         let c = HcGreekVerbForm {verb:&a, person:HcPerson::First, number:HcNumber::Singular, tense:HcTense::Aorist, voice:HcVoice::Active, mood:HcMood::Indicative, gender:None};
         assert_eq!(b, c);
-        assert_eq!(b.get_form()[0].form, "blah");
+        assert_eq!(b.get_form()[0].form, "λω, λσω, ἔλῡσα, λέλυκα, λέλυμαι, ἐλύθην");
+        assert_eq!(b.get_form()[1].form, "ἔλῡσα");
+        assert_eq!(b.get_form()[2].form, "ἔλῡσ");
         assert_eq!(b.get_pp_num(), HcGreekPrincipalParts::Third);
         assert_eq!(b.get_pp_num() as usize, 3);
         assert_eq!(b.verb.pps[b.get_pp_num() as usize - 1], "ἔλῡσα");
